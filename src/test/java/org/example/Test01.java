98b299dbaa64f558e4993f78cc7087f6e2efcaf4
kanglong <535991297@qq.com>
2021-08-17 14:32:40 +0800

    修改test01

--- src/test/java/org/example/Test01.java
+++ src/test/java/org/example/Test01.java
@@ -3,5 +3,8 @@ package org.example;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("test...");
+        System.out.println("test...");
+        System.out.println("test...");
+        System.out.println("test...");
     }
 }
